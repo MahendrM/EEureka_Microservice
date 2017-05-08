cb2dc9a83060906b42d75b8495413395a6df8e9a
newtglobaluser <newtglobaluser@gmail.com>
2017-05-08 14:27:36 +0530

    Update EurekaServerProjectApplication.java

--- src/main/java/com/example/EurekaServerProjectApplication.java
+++ src/main/java/com/example/EurekaServerProjectApplication.java
@@ -4,12 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
-
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerProjectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerProjectApplication.class, args);
 	}	
