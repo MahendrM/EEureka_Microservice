581c24e8c79260f0c4fa510a51c88d2b6aaa9e4e
newtglobaluser <newtglobaluser@gmail.com>
2017-05-08 16:13:20 +0530

    Update EurekaCont.java

--- src/main/java/com/example/EurekaCont.java
+++ src/main/java/com/example/EurekaCont.java
@@ -3,6 +3,8 @@ package com.example;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 public class EurekaCont {
 
